b4bc215f9e6eda2d199b8e0ec6d97becb77ed966
beno1234 <benolopesdias@gmail.com>
2023-07-27 17:06:10 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require("express"); //backend geral
+const app = express();
+const cors = require("cors");
+const nodemailer = require("nodemailer"); //integração smtp email
+const port = process.env.PORT || 4000;
+
+app.use(express.json());
+app.use(cors());
+
+app.post("/enviar-email", async (req, res) => {
+  const { nome, email, telefone, mensagem } = req.body;
+
+  let transporter = nodemailer.createTransport({
+    host: "smtp-mail.outlook.com",
+    port: 587,
+    secure: false, // true for 465, false for other ports
+    auth: {
+      user: "contatopets@petsmellon.com.br",
+      pass: "Patense2438",
+    },
+  });
+
+  let info = await transporter.sendMail({
+    from: "contatopets@petsmellon.com.br",
+    to: "benolopesdias@gmail.com",
+    subject: "Contato pela Landing Page",
+    html: `
+        <p><strong>Nome:</strong> ${nome}</p>
+        <p><strong>Email:</strong> ${email}</p>
+        <p><strong>Telefone:</strong> ${telefone}</p>
+        <p><strong>Mensagem:</strong> ${mensagem}</p>
+      `,
+  });
+
+  console.log("Message sent: %s", info.messageId);
+  console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+
+  res.sendStatus(200);
+});
+
+app.listen(port, () => {
+  console.log(`RODANDO NA PORTA ${port}`);
+});
